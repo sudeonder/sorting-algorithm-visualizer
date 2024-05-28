a3956ee0d183c8bfe5d84235c3b6edd965a35710
Sude Önder <sudeonder999@gmail.com>
2024-05-28 20:50:07 +0300

    implement draw bar function

--- visualizor.py
+++ visualizor.py
@@ -11,6 +11,11 @@ class DrawInformation:
     BACKGROUND_COLOR = WHITE
     SIDE_PADDING = 50
     TOP_PADDING = 150
+    BAR_GRADIENTS = [
+        (245, 183, 177),
+        (230, 176, 170),
+        (241, 148, 138)
+    ]
 
     def __init__(self, width, height, lst):
         self.width = width
@@ -33,18 +38,42 @@ def generate_list(n, min_val, max_val):
     return [random.randint(min_val, max_val) for _ in range(n)]
 
 
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    draw_bars(draw_info)
+    pygame.display.update()
+
+
+def draw_bars(draw_info):
+
+    for i, val in enumerate(draw_info.lst):
+        x = draw_info.start_x + i * draw_info.bar_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.bar_height
+        color = draw_info.BAR_GRADIENTS[i % 3]
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.bar_width, draw_info.height))
+
+    
+
+
 def main():
     run = True
     clock = pygame.time.Clock()
 
+    n = 50
+    min_val = 1
+    max_val = 100
+    lst = generate_list(n, min_val, max_val)
+    draw_info = DrawInformation(800, 600, lst)
+
     while run:
         clock.tick(60)
+        draw(draw_info)
 
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
+
     pygame.quit()
 
 if __name__ == "__main__":
