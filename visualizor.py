78c69223604a81531364f81492c83f6de6e1b5e8
Sude Önder <sudeonder999@gmail.com>
2024-05-29 14:00:09 +0300

    implement recreating list

--- visualizor.py
+++ visualizor.py
@@ -74,6 +74,13 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+            if event.type != pygame.KEYDOWN:
+                continue
+
+            if event.key == pygame.K_r:
+                lst = generate_list(n, min_val, max_val)
+                draw_info.set_lst(lst)
+
     pygame.quit()
 
 if __name__ == "__main__":
